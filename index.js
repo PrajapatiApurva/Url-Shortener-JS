27d9c6e61e4dc747a981bab67e9f6ee102573591
apurva-6265 <apurva18092003@gmail.com>
2024-05-17 10:52:13 +0530

    Resolved The Link Post Issue

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const mongoose=require('mongoose')
-const shortUrl=require('short-unique-id');
+const ShortUniqueId=require('short-unique-id');
 const bodyParser=require('body-parser');
 
 //Creating Object of express
 const app=express();
 //initializing Port number
 const PORT=5501;
-
+const URL=require('./models/urlModel');
 //Middleware
 app.use(bodyParser.json());
 
@@ -25,11 +25,11 @@ const {connectDB} = require('./mongoDb')
 connectDB();
 
 //Importing MongoDB models
-const URL=require('./models/urlModel');
+
 
 //Methods
 const isValidUrl = url => {
-    const urlRegex = /^(http|https):\/\/[\w.-]+(?:[:\d]*)\/\S+$/;
+    const urlRegex = /^(http|https):\/\/[\w.-_~:\/?#[\]@!$&'()*+,;=]+$/;
     return urlRegex.test(url);
 }
 
@@ -53,19 +53,21 @@ app.get('/contact',(req,res)=>{
 app.post('/shorten',async(req,res)=>{
     const {fullUrl} = req.body;
 
-    // if(!isValidUrl(fullUrl)){
-    //     return res.json({message:"Invalid Url"});
-    // }
+    if(!isValidUrl(fullUrl)){
+        return res.json({message:"Invalid Url"});
+    }
     try{
         const existingUrl=await URL.findOne({fullUrl});
         if(existingUrl){
             return res.json({shortUrl:existingUrl.shortUrl});
         }
         
-        const shortUrl = shortUrl.generate();
-        const newUrl= new URL({fullUrl,shortUrl});
+        const uid =new ShortUniqueId({length:15});
+        const newUrlGen= uid.rnd();
+        // console.log(newUrlGen);
+        const newUrl=URL({fullUrl,shortUrl:newUrlGen});
         await newUrl.save();
-        res.json({shortUrl});
+        res.json({newUrlGen});
         
     }
     catch(err){
